6e9fb5868819dd8fb55703e6b233dc1919a8bd61
veronnah <yoo.veroon@gmail.com>
2022-03-14 22:03:26 +0100

    desktop version done

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+function scrollUp() {
+    const scrollUp = document.getElementById('scroll-up');
+    if (this.scrollY >= 200) {
+        scrollUp.classList.add('show-scroll');
+    } else {
+        scrollUp.classList.remove('show-scroll');
+    }
+}
+window.addEventListener('scroll', scrollUp);
